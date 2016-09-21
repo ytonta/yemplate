b7aa0a2e6e54cbba0e8c7f5ec76b17caa674bf7a
Yannick Tonta <tontay@proximity.bbdo.fr>
2016-09-21 11:41:54 +0400

    Added webpack and gulp rename.

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,64 @@
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var pug          = require('gulp-pug');
-var nodemon      = require('gulp-nodemon')
-var browserSync  = require('browser-sync').create();
-var reload       = browserSync.reload;
-var htmlInjector = require('bs-html-injector');
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var pug           = require('gulp-pug');
+var nodemon       = require('gulp-nodemon')
+var rename        = require('gulp-rename');
+var browserSync   = require('browser-sync').create();
+var reload        = browserSync.reload;
+var htmlInjector  = require('bs-html-injector');
+var webpack       = require('webpack-stream');
 
-var paths = {
-  scripts: './app/scripts/**/*.js',
-  pug: './app/views/**/*.pug',
-  sass: './app/sass/**/*.scss',
-  css: './dist/css',
-  html: './dist/html'
+var config = {
+  html: {
+    inputDir: './app/views/**/*.pug',
+    outputDir: './dist/html'
+  },
+  css: {
+    inputDir: './app/sass/**/*.scss',
+    outputDir: './dist/css',
+    outputFile: 'main.css'
+  },
+  scripts: {
+    inputDir: './app/scripts/**/*.js',
+    outputDir: './dist/js',
+    outputFile: 'main.js'
+  }
 };
 
 // Compiles the pug to html
 gulp.task('views', function () {
-  return gulp.src(paths.pug)
+  return gulp.src(config.html.inputDir)
     .pipe(pug())
-    .pipe(gulp.dest(paths.html));
+    .pipe(gulp.dest(config.html.outputDir));
 });
 
 // Compiles the sass to css
 gulp.task('sass', function () {
-  return gulp.src(paths.sass)
+  return gulp.src(config.css.inputDir)
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(paths.css))
+    .pipe(rename('main.css'))
+    .pipe(gulp.dest(config.css.outputDir))
     .pipe(reload({ stream: true }));
+});
+
+// Webpack function, bundles the js
+gulp.task('js', function() {
+  return gulp.src(config.scripts.inputDir)
+    .pipe(webpack( require('./webpack.config.js') ))
+    .pipe(rename('main.js'))
+    .pipe(gulp.dest(config.scripts.outputDir));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.pug, ['views']);
+  gulp.watch(config.css.inputDir, ['sass']);
+  gulp.watch(config.html.inputDir, ['views']);
 });
 
 // Start the server
 gulp.task('nodemon', function () {
   nodemon({
     script: 'app.js'
-    , ext: 'js'
     , env: { 'NODE_ENV': 'development' }
   });
 });
@@ -47,7 +66,7 @@ gulp.task('nodemon', function () {
 // Start browsersync
 gulp.task('start', ['nodemon'], function () {
   browserSync.use(htmlInjector, {
-    files: paths.html
+    files: config.html.outputDir
   });
   browserSync.init(null, {
     proxy: 'http://localhost:3000',
@@ -56,4 +75,4 @@ gulp.task('start', ['nodemon'], function () {
 });
 
 // Default task
-gulp.task('default', ['start', 'watch', 'views', 'sass'], function() {});
+gulp.task('default', ['start', 'watch', 'views', 'sass', 'js'], function() {});
