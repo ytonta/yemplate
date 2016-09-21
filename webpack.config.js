10342798295563d62dea4346897b180d0e93fb91
Yannick Tonta <tontay@proximity.bbdo.fr>
2016-09-21 13:05:58 +0400

    Added babel.

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const webpack = require('webpack');
+
+module.exports = {
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+      output: {
+        comments: false,
+      },
+    }),
+  ]
+}
